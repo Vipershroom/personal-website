5f11e24221df2f0c2fac7e0e90b7a3ee0dcffafb
Vipershroom <undertalefan812@gmail.com>
2022-02-23 01:28:40 -0500

    Added id to darkmode tag

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 let timeEl = document.getElementById('time-el')
+let darkMode = document.getElementById('dark-mode')
+
 
 setInterval(() => {
     timeEl!.innerText = `Your current time is: ${new Date().toLocaleTimeString()}`
