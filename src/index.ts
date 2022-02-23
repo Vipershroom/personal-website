5234dea28c7a861b0d544342e6f5aca07b81a655
Vipershroom <undertalefan812@gmail.com>
2022-02-23 01:13:56 -0500

    Added a timer to the header

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("Hello World!")
+let timeEl = document.getElementById('time-el')
+
+setInterval(() => {
+    timeEl!.innerText = `Your current time is: ${new Date().toLocaleTimeString()}`
+}, 1000)
