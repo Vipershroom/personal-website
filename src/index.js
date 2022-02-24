720e3c7e1de230d6f0e1c3f2757f12eb201304c0
Vipershroom <undertalefan812@gmail.com>
2022-02-24 02:00:16 -0500

    Moved files into docs and updated css

--- src/index.js
+++
@@ -1,5 +0,0 @@
-var timeEl = document.getElementById('time-el');
-var darkMode = document.getElementById('dark-mode');
-setInterval(function () {
-    timeEl.innerText = "Your current time is: ".concat(new Date().toLocaleTimeString());
-}, 1000);
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-let timeEl = document.getElementById('time-el')
-let darkMode = document.getElementById('dark-mode')
-
-
-setInterval(() => {
-    timeEl!.innerText = `Your current time is: ${new Date().toLocaleTimeString()}`
-}, 1000)
